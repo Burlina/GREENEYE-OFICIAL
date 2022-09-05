9790f89d2359f66b0702be356827aaec9cd07cce
pedro <morettipinsan@gmail.com>
2022-09-05 12:41:01 -0300

    Rotas arrumadas

--- src/route/pageRoute.js
+++ src/route/pageRoute.js
@@ -12,4 +12,16 @@ router.get('/login', (req, res) => {
     res.sendFile(path.resolve('public/view/login.html'))
 })
 
+router.get('/cadastroempresa', (req, res) => {
+    res.sendFile(path.resolve('public/view/cadastroEmpresa.html'))
+})
+
+router.get('/cadastrousuario', (req, res) => {
+    res.sendFile(path.resolve('public/view/cadastroUsuario.html'))
+})
+
+router.get('/calculadora', (req, res) => {
+    res.sendFile(path.resolve('public/view/calculadora.html'))
+})
+
 export default router
