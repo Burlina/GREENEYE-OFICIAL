08cebf5b68f552858801f1360f3fe829c6614d53
ThaisFrancaI <thais.inacio@sptech.school>
2022-10-25 14:57:58 -0300

    Dashboard e webcrawler funcionando

--- src/controllers/medidaController.cjs
+++ src/controllers/medidaController.cjs
@@ -5,7 +5,7 @@ function buscarUltimasMedidas(req, res) {
     const limite_linhas = 7;
 
     var idAquario = req.params.cpuMedia;
-
+    
     console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
 
     medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
@@ -24,12 +24,15 @@ function buscarUltimasMedidas(req, res) {
 
 function buscarMedidasEmTempoReal(req, res) {
 
-    var idAquario = req.params.cpuMedia;
+    var idAquario = req.params.idArmazem;
 
+    console.log("ID", idAquario);
+    
     console.log(`Recuperando medidas em tempo real`);
 
     medidaModel.buscarMedidasEmTempoReal(idAquario).then(function (resultado) {
         if (resultado.length > 0) {
+            console.log("RESULTADO", resultado);
             res.status(200).json(resultado);
         } else {
             res.status(204).send("Nenhum resultado encontrado!")
--- src/models/medidaModel.cjs
+++ src/models/medidaModel.cjs
@@ -28,6 +28,7 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     return database.executar(instrucaoSql);
 }
 
+
 function buscarMedidasEmTempoReal(idAquario) {
 
     instrucaoSql = ''
@@ -42,8 +43,44 @@ function buscarMedidasEmTempoReal(idAquario) {
     order by idRegistros desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        let coleta = ""
+        if (idAquario == 1) {
+            coleta = "cpuMedia"
+        }
+        else if (idAquario == 2) {
+            coleta = "ramUsoPercent"
+        }
+
         instrucaoSql = `select 
-        cpuMedia, 
+        ${coleta}, 
+        date_format(dataHora, '%H:%i:%s') as momento_grafico 
+    from Leitura  
+    order by idLeitura desc limit 1`;
+
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal1(idMemoria) {
+
+    instrucaoSql1 = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top 1 
+        REGISTRO_TEMP, 
+        REGISTRO_UMID, 
+        REGISTRO_MOMENTO,
+        CONVERT(varchar, REGISTRO_MOMENTO, 108) as momento_grafico
+    from registros  
+    order by idRegistros desc`;
+
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select ramUsoPercent, 
         date_format(dataHora, '%H:%i:%s') as momento_grafico 
     from Leitura  
     order by date_format(dataHora, '%H:%i:%s') desc limit 1`;
@@ -53,8 +90,8 @@ function buscarMedidasEmTempoReal(idAquario) {
         return
     }
 
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+    console.log("Executando a instrução SQL: \n" + instrucaoSql1);
+    return database.executar(instrucaoSql1);
 }
 
 
